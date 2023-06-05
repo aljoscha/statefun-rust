e97dd8fe20bb0ae1fe755647e9d2f14155e340c7
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2023-06-05 14:40:08 +0900

    Update examples to work with flink-statefun v3.2.0
    This also updates Rust to the latest version.
    
    The examples were refactored to match the ones in the
    statefun-playground.

--- statefun-greeter-example/README.md
+++ statefun-greeter-example/README.md
@@ -15,13 +15,23 @@ does is package messages to send them to the Kafka Egress.
 To run the example:
 
 ```
-docker-compose up -d
+docker-compose up --build
 ```
 
-Then, to see the example in actions, see what comes out of the topic
-`greetings`:
+## Play around!
+
+The greeter application allows you to do the following actions:
+
+* Create a greeting for a user via sending a `UserLogin` message to the `user` function
+
+In order to send messages to the Stateful Functions application you can run:
 
 ```
-docker-compose logs -f datagen
+$ curl -X PUT -H "Content-Type: application/vnd.greeter.types/UserLogin" -d '{"user_id": "1", "user_name": "Joe", "login_type": "WEB"}' localhost:8090/greeter.fns/user/1
 ```
 
+You can take a look at what messages are being sent to the Playground egress:
+
+```
+$ curl -X GET localhost:8091/greetings
+```
--- statefun-greeter-example/src/main.rs
+++ statefun-greeter-example/src/main.rs
@@ -1,72 +1,101 @@
 use protobuf::well_known_types::Int32Value;
+use protobuf::well_known_types::Int64Value;
 
 use statefun::io::kafka::KafkaEgress;
 use statefun::transport::hyper::HyperHttpTransport;
 use statefun::transport::Transport;
 use statefun::{Address, Context, Effects, EgressIdentifier, FunctionRegistry, FunctionType};
-use statefun_greeter_example_proto::example::GreetRequest;
-use statefun_greeter_example_proto::example::GreetResponse;
-
-pub fn greet(context: Context, request: GreetRequest) -> Effects {
-    log::info!("We should greet {:?}", request.get_name());
-
-    let seen_count: Option<Int32Value> = context.get_state("seen_count");
-
-    log::info!(
-        "We have seen {:?} {:?} times.",
-        request.get_name(),
-        seen_count
-    );
-
-    let mut effects = Effects::new();
-
-    let mut updated_seen_count = match seen_count {
-        Some(count) => count,
-        None => Int32Value::new(),
-    };
-    updated_seen_count.set_value(updated_seen_count.get_value() + 1);
-    effects.update_state("seen_count", &updated_seen_count);
-
-    let mut greet_response = GreetResponse::new();
-    greet_response.set_name(request.get_name().to_owned());
-    greet_response.set_greeting(format!(
-        "Say hello to {} from Rust. I've seen them {} times now.",
-        request.get_name(),
-        updated_seen_count.get_value()
-    ));
-
-    effects.send(
-        Address::new(FunctionType::new("example", "relay"), request.get_name()),
-        greet_response,
-    );
-
-    effects
-}
-
-pub fn relay(_context: Context, message: GreetResponse) -> Effects {
-    log::info!("Relaying message {:?} to Kafka.", message);
-
-    let mut effects = Effects::new();
-
-    effects.kafka_keyed_egress(
-        EgressIdentifier::new("example", "greets"),
-        "greetings",
-        &message.get_name().to_string(),
-        message,
-    );
-
-    effects
-}
+use statefun_greeter_example_proto::example::UserLogin;
+use statefun_greeter_example_proto::example::UserProfile;
+use statefun_greeter_example_proto::example::EgressRecord;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 fn main() -> anyhow::Result<()> {
     env_logger::init();
 
     let mut function_registry = FunctionRegistry::new();
-    function_registry.register_fn(FunctionType::new("example", "greeter"), greet);
-    function_registry.register_fn(FunctionType::new("example", "relay"), relay);
+    function_registry.register_fn(FunctionType::new("greeter.fns", "user"), user);
+    function_registry.register_fn(FunctionType::new("greeter.fns", "greetings"), greet);
 
-    let hyper_transport = HyperHttpTransport::new("0.0.0.0:5000".parse()?);
+    let hyper_transport = HyperHttpTransport::new("0.0.0.0:1108".parse()?);
     hyper_transport.run(function_registry)?;
 
     Ok(())
+}
+
+pub fn user(context: Context, login: UserLogin) -> Effects {
+    log::info!("We should update user count {:?}", login.get_user_name());
+
+    let seen_count: Option<Int32Value> = context.get_state("seen_count");
+    let mut updated_seen_count = match seen_count {
+        Some(count) => count,
+        None => Int32Value::new(),
+    };
+    updated_seen_count.set_value(updated_seen_count.get_value() + 1);
+
+    let start = SystemTime::now();
+    let since_the_epoch = start
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards");
+    let now_ms = since_the_epoch.as_millis() as i64;
+
+    let last_seen_timestamp_ms : Option<Int64Value> = context.get_state("seen_timestamp_ms");
+    let mut updated_last_seen_timestamp_ms = match last_seen_timestamp_ms {
+        Some(last_seen) => last_seen,
+        None => { let mut x = Int64Value::new(); x.set_value(now_ms); x },
+    };
+
+    let mut effects = Effects::new();
+    effects.update_state("seen_count", &updated_seen_count);
+    effects.update_state("seen_timestamp_ms", &updated_last_seen_timestamp_ms);
+
+    log::info!(
+        "We have seen {:?} {:?} times.",
+        login.get_user_name(),
+        updated_seen_count
+    );
+
+    let mut profile = UserProfile::new();
+    profile.set_name(login.get_user_name().to_string());
+    profile.set_last_seen_delta_ms(now_ms);
+    profile.set_login_location(format!("{:?}", login.get_login_type()));
+    profile.set_seen_count(updated_seen_count.value);
+
+    effects.send(
+        Address::new(FunctionType::new("greeter.fns", "greetings"), login.get_user_name()),
+        profile,
+    );
+
+    effects
+}
+
+pub fn greet(_context: Context, profile: UserProfile) -> Effects {
+    log::info!("We should greet {:?}", profile.get_name());
+
+    let mut effects = Effects::new();
+    let greetings = createGreetingsMessage(profile);
+
+    let mut egressRecord = EgressRecord::new();
+    egressRecord.set_topic("greetings".to_string());
+    egressRecord.set_payload(greetings);
+
+    effects.egress(EgressIdentifier::new("io.statefun.playground", "egress"),
+                   egressRecord);
+
+    effects
+}
+
+pub fn createGreetingsMessage(profile: UserProfile) -> String {
+    let GREETINGS_TEMPLATES =
+      ["Welcome", "Nice to see you again", "Third time is a charm"];
+
+    let seenCount = profile.get_seen_count() as usize;
+
+    if seenCount <= GREETINGS_TEMPLATES.len() {
+      return format!("{:?} {:?}.", GREETINGS_TEMPLATES[seenCount], profile.get_name());
+    } else {
+      return format!(
+        "Nice to see you for the {:?}th time, {:?}! It has been {:?} milliseconds since we last saw you.",
+          seenCount, profile.get_name(), profile.get_last_seen_delta_ms());
+    }
 }
