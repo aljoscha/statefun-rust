8640e527cc0236a6f03666eb13d2d6535a9d2f4c
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2023-06-09 11:55:09 +0900

    Fixup examples again

--- statefun-greeter-example/src/main.rs
+++ statefun-greeter-example/src/main.rs
@@ -8,6 +8,7 @@ use statefun::{Address, Context, Effects, EgressIdentifier, FunctionRegistry, Fu
 use statefun_greeter_example_proto::example::UserLogin;
 use statefun_greeter_example_proto::example::UserProfile;
 use statefun_greeter_example_proto::example::EgressRecord;
+use statefun_proto::request_reply::TypedValue;
 use std::time::{SystemTime, UNIX_EPOCH};
 
 fn main() -> anyhow::Result<()> {
@@ -23,64 +24,72 @@ fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-pub fn user(context: Context, login: UserLogin) -> Effects {
-    log::info!("We should update user count {:?}", login.get_user_name());
+pub fn user(context: Context, typed_value: TypedValue) -> Effects {
+    log::info!("--user: received: {:?}", typed_value);
+    // todo: fixup
+    // login: UserLogin
 
-    let seen_count: Option<Int32Value> = context.get_state("seen_count");
-    let mut updated_seen_count = match seen_count {
-        Some(count) => count,
-        None => Int32Value::new(),
-    };
-    updated_seen_count.set_value(updated_seen_count.get_value() + 1);
+    // log::info!("We should update user count {:?}", login.get_user_name());
 
-    let start = SystemTime::now();
-    let since_the_epoch = start
-        .duration_since(UNIX_EPOCH)
-        .expect("Time went backwards");
-    let now_ms = since_the_epoch.as_millis() as i64;
+    // let seen_count: Option<Int32Value> = context.get_state("seen_count");
+    // let mut updated_seen_count = match seen_count {
+    //     Some(count) => count,
+    //     None => Int32Value::new(),
+    // };
+    // updated_seen_count.set_value(updated_seen_count.get_value() + 1);
 
-    let last_seen_timestamp_ms : Option<Int64Value> = context.get_state("seen_timestamp_ms");
-    let mut updated_last_seen_timestamp_ms = match last_seen_timestamp_ms {
-        Some(last_seen) => last_seen,
-        None => { let mut x = Int64Value::new(); x.set_value(now_ms); x },
-    };
+    // let start = SystemTime::now();
+    // let since_the_epoch = start
+    //     .duration_since(UNIX_EPOCH)
+    //     .expect("Time went backwards");
+    // let now_ms = since_the_epoch.as_millis() as i64;
+
+    // let last_seen_timestamp_ms : Option<Int64Value> = context.get_state("seen_timestamp_ms");
+    // let mut updated_last_seen_timestamp_ms = match last_seen_timestamp_ms {
+    //     Some(last_seen) => last_seen,
+    //     None => { let mut x = Int64Value::new(); x.set_value(now_ms); x },
+    // };
 
     let mut effects = Effects::new();
-    effects.update_state("seen_count", &updated_seen_count);
-    effects.update_state("seen_timestamp_ms", &updated_last_seen_timestamp_ms);
+    // effects.update_state("seen_count", &updated_seen_count);
+    // effects.update_state("seen_timestamp_ms", &updated_last_seen_timestamp_ms);
 
-    log::info!(
-        "We have seen {:?} {:?} times.",
-        login.get_user_name(),
-        updated_seen_count
-    );
+    // log::info!(
+    //     "We have seen {:?} {:?} times.",
+    //     login.get_user_name(),
+    //     updated_seen_count
+    // );
 
-    let mut profile = UserProfile::new();
-    profile.set_name(login.get_user_name().to_string());
-    profile.set_last_seen_delta_ms(now_ms);
-    profile.set_login_location(format!("{:?}", login.get_login_type()));
-    profile.set_seen_count(updated_seen_count.value);
+    // let mut profile = UserProfile::new();
+    // profile.set_name(login.get_user_name().to_string());
+    // profile.set_last_seen_delta_ms(now_ms);
+    // profile.set_login_location(format!("{:?}", login.get_login_type()));
+    // profile.set_seen_count(updated_seen_count.value);
 
-    effects.send(
-        Address::new(FunctionType::new("greeter.fns", "greetings"), login.get_user_name()),
-        profile,
-    );
+    // effects.send(
+    //     Address::new(FunctionType::new("greeter.fns", "greetings"), login.get_user_name()),
+    //     profile,
+    // );
 
     effects
 }
 
-pub fn greet(_context: Context, profile: UserProfile) -> Effects {
-    log::info!("We should greet {:?}", profile.get_name());
+pub fn greet(_context: Context, typed_value: TypedValue) -> Effects {
+    log::info!("--greet: Received {:?}", typed_value);
+    // todo:
+    // profile: UserProfile
+
+    // log::info!("We should greet {:?}", profile.get_name());
 
     let mut effects = Effects::new();
-    let greetings = createGreetingsMessage(profile);
+    // let greetings = createGreetingsMessage(profile);
 
-    let mut egressRecord = EgressRecord::new();
-    egressRecord.set_topic("greetings".to_string());
-    egressRecord.set_payload(greetings);
+    // let mut egressRecord = EgressRecord::new();
+    // egressRecord.set_topic("greetings".to_string());
+    // egressRecord.set_payload(greetings);
 
-    effects.egress(EgressIdentifier::new("io.statefun.playground", "egress"),
-                   egressRecord);
+    // effects.egress(EgressIdentifier::new("io.statefun.playground", "egress"),
+    //                egressRecord);
 
     effects
 }
