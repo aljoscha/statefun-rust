9ed5b0c88de143b99fb4b4a1bb4d63fffabdc9ee
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2023-06-13 02:46:46 +0900

    Move missing state collection

--- statefun-sdk/src/lib.rs
+++ statefun-sdk/src/lib.rs
@@ -89,30 +89,11 @@ mod effects;
 pub use effects::Effects;
 mod function_registry;
 mod invocation_bridge;
+mod missing_state_collection;
+use missing_state_collection::MissingStateCollection;
 pub mod io;
 pub mod transport;
 use statefun_proto::request_reply::TypedValue;
-
-/// blabla
-#[derive(Error, PartialEq, Eq, Hash, Debug)]
-pub struct MissingStateCollection {
-    states: Vec<ValueSpecBase>,
-}
-
-impl MissingStateCollection {
-    /// blabla
-    pub fn new(states: Vec<ValueSpecBase>) -> MissingStateCollection {
-        MissingStateCollection {
-            states: states,
-        }
-    }
-}
-
-impl Display for MissingStateCollection {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "MissingStateCollection {:?}", self.states)
-    }
-}
 
 ///
 #[derive(Debug)]
---
+++ statefun-sdk/src/missing_state_collection.rs
@@ -0,0 +1,24 @@
+use crate::ValueSpecBase;
+use std::fmt::{Display, Formatter};
+use thiserror::Error;
+
+/// blabla
+#[derive(Error, PartialEq, Eq, Hash, Debug)]
+pub struct MissingStateCollection {
+    pub (crate) states: Vec<ValueSpecBase>,
+}
+
+impl MissingStateCollection {
+    /// blabla
+    pub fn new(states: Vec<ValueSpecBase>) -> MissingStateCollection {
+        MissingStateCollection {
+            states: states,
+        }
+    }
+}
+
+impl Display for MissingStateCollection {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "MissingStateCollection {:?}", self.states)
+    }
+}
