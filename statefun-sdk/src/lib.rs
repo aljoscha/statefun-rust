140af5552fd030b6748cae19184b7a1686922094
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2023-06-13 02:36:03 +0900

    Move leftover serialization routines

--- statefun-sdk/src/lib.rs
+++ statefun-sdk/src/lib.rs
@@ -66,6 +66,7 @@ use statefun_proto::request_reply::Address as ProtoAddress;
 use statefun_proto::types::{BooleanWrapper, IntWrapper, LongWrapper};
 
 mod serialization;
+use serialization::{serializer, deserializer};
 pub use serialization::Serializable;
 mod function_type;
 pub use function_type::FunctionType;
@@ -166,71 +167,6 @@ impl<T> Into<ValueSpecBase> for ValueSpec<T> {
     fn into(self) -> ValueSpecBase {
         ValueSpecBase::new(self.name.to_string().as_str(), self.typename.to_string().as_str())
     }
-}
-
-impl Serializable for bool {
-    fn serialize(&self, typename: String) -> Vec<u8> {
-        let mut wrapped = BooleanWrapper::new();
-        wrapped.set_value(*self);
-        wrapped.write_to_bytes().unwrap()
-    }
-
-    fn deserialize(typename: String, buffer: &Vec<u8>) -> bool {
-        let wrapped = parse_from_bytes::<BooleanWrapper>(&buffer).unwrap();
-        wrapped.get_value()
-    }
-}
-
-impl Serializable for i32 {
-    fn serialize(&self, typename: String) -> Vec<u8> {
-        let mut wrapped = IntWrapper::new();
-        log::debug!("-- drey: i32 serializing {:?}", self);
-        wrapped.set_value(*self);
-        log::debug!("-- drey: wrapped {:?}", wrapped);
-        let res = wrapped.write_to_bytes().unwrap();
-        log::debug!("-- drey: res {:?}", res);
-
-        res
-    }
-
-    fn deserialize(typename: String, buffer: &Vec<u8>) -> i32 {
-        let wrapped = parse_from_bytes::<IntWrapper>(&buffer).unwrap();
-        wrapped.get_value()
-    }
-}
-
-impl Serializable for i64 {
-    fn serialize(&self, typename: String) -> Vec<u8> {
-        let mut wrapped = LongWrapper::new();
-        log::debug!("-- drey: i32 serializing {:?}", self);
-        wrapped.set_value(*self);
-        log::debug!("-- drey: wrapped {:?}", wrapped);
-        let res = wrapped.write_to_bytes().unwrap();
-        log::debug!("-- drey: res {:?}", res);
-
-        res
-    }
-
-    fn deserialize(typename: String, buffer: &Vec<u8>) -> i64 {
-        let wrapped = parse_from_bytes::<LongWrapper>(&buffer).unwrap();
-        wrapped.get_value()
-    }
-}
-
-fn serializer<T : Serializable>(value: &T, typename: String) -> Vec<u8> {
-    // log::debug!("-- drey: serializing type: {:?}", typename);
-    (&value).serialize(typename)
-    // log::debug!("-- drey: serialized to: {:?}", &res);
-}
-
-// todo
-fn deserializer<T : Serializable>(typename: String, buffer: &Vec<u8>) -> T {
-    // log::debug!("-- drey: deserializing type: {:?}", typename);
-    // todo: how do we limit T here so T::new will work??
-    // T::new()
-    // panic!("oops")
-
-    T::deserialize(typename, buffer)
 }
 
 impl<T: Serializable> ValueSpec<T> {
--- statefun-sdk/src/serialization.rs
+++ statefun-sdk/src/serialization.rs
@@ -1,3 +1,6 @@
+use statefun_proto::types::{BooleanWrapper, IntWrapper, LongWrapper};
+use protobuf::parse_from_bytes;
+use protobuf::Message;
 
 ///
 pub trait Serializable {
@@ -6,4 +9,69 @@ pub trait Serializable {
 
     ///
     fn deserialize(typename: String, buffer: &Vec<u8>) -> Self;
+}
+
+impl Serializable for bool {
+    fn serialize(&self, typename: String) -> Vec<u8> {
+        let mut wrapped = BooleanWrapper::new();
+        wrapped.set_value(*self);
+        wrapped.write_to_bytes().unwrap()
+    }
+
+    fn deserialize(typename: String, buffer: &Vec<u8>) -> bool {
+        let wrapped = parse_from_bytes::<BooleanWrapper>(&buffer).unwrap();
+        wrapped.get_value()
+    }
+}
+
+impl Serializable for i32 {
+    fn serialize(&self, typename: String) -> Vec<u8> {
+        let mut wrapped = IntWrapper::new();
+        log::debug!("-- drey: i32 serializing {:?}", self);
+        wrapped.set_value(*self);
+        log::debug!("-- drey: wrapped {:?}", wrapped);
+        let res = wrapped.write_to_bytes().unwrap();
+        log::debug!("-- drey: res {:?}", res);
+
+        res
+    }
+
+    fn deserialize(typename: String, buffer: &Vec<u8>) -> i32 {
+        let wrapped = parse_from_bytes::<IntWrapper>(&buffer).unwrap();
+        wrapped.get_value()
+    }
+}
+
+impl Serializable for i64 {
+    fn serialize(&self, typename: String) -> Vec<u8> {
+        let mut wrapped = LongWrapper::new();
+        log::debug!("-- drey: i32 serializing {:?}", self);
+        wrapped.set_value(*self);
+        log::debug!("-- drey: wrapped {:?}", wrapped);
+        let res = wrapped.write_to_bytes().unwrap();
+        log::debug!("-- drey: res {:?}", res);
+
+        res
+    }
+
+    fn deserialize(typename: String, buffer: &Vec<u8>) -> i64 {
+        let wrapped = parse_from_bytes::<LongWrapper>(&buffer).unwrap();
+        wrapped.get_value()
+    }
+}
+
+pub (crate) fn serializer<T : Serializable>(value: &T, typename: String) -> Vec<u8> {
+    // log::debug!("-- drey: serializing type: {:?}", typename);
+    (&value).serialize(typename)
+    // log::debug!("-- drey: serialized to: {:?}", &res);
+}
+
+// todo
+pub (crate) fn deserializer<T : Serializable>(typename: String, buffer: &Vec<u8>) -> T {
+    // log::debug!("-- drey: deserializing type: {:?}", typename);
+    // todo: how do we limit T here so T::new will work??
+    // T::new()
+    // panic!("oops")
+
+    T::deserialize(typename, buffer)
 }
