477eb4660d7b579c06f2b08e222e246af4f105ca
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2023-06-14 14:50:26 +0900

    Add support for float / double serialization

--- statefun-sdk/src/serialization.rs
+++ statefun-sdk/src/serialization.rs
@@ -1,7 +1,9 @@
 // use protobuf::parse_from_bytes;
 use crate::Serializable;
 use protobuf::Message;
-use statefun_proto::types::{BooleanWrapper, IntWrapper, LongWrapper, StringWrapper};
+use statefun_proto::types::{
+    BooleanWrapper, DoubleWrapper, FloatWrapper, IntWrapper, LongWrapper, StringWrapper,
+};
 
 impl Serializable<bool> for bool {
     fn serialize(&self, _typename: String) -> Result<Vec<u8>, String> {
@@ -48,6 +50,38 @@ impl Serializable<i64> for i64 {
 
     fn deserialize(_typename: String, buffer: &Vec<u8>) -> Result<i64, String> {
         match LongWrapper::parse_from_bytes(buffer) {
+            Ok(result) => Ok(result.get_value()),
+            Err(result) => Err(result.to_string()),
+        }
+    }
+}
+
+impl Serializable<f32> for f32 {
+    fn serialize(&self, _typename: String) -> Result<Vec<u8>, String> {
+        let mut wrapped = FloatWrapper::new();
+        wrapped.set_value(*self);
+        let res = wrapped.write_to_bytes().unwrap();
+        Ok(res)
+    }
+
+    fn deserialize(_typename: String, buffer: &Vec<u8>) -> Result<f32, String> {
+        match FloatWrapper::parse_from_bytes(buffer) {
+            Ok(result) => Ok(result.get_value()),
+            Err(result) => Err(result.to_string()),
+        }
+    }
+}
+
+impl Serializable<f64> for f64 {
+    fn serialize(&self, _typename: String) -> Result<Vec<u8>, String> {
+        let mut wrapped = DoubleWrapper::new();
+        wrapped.set_value(*self);
+        let res = wrapped.write_to_bytes().unwrap();
+        Ok(res)
+    }
+
+    fn deserialize(_typename: String, buffer: &Vec<u8>) -> Result<f64, String> {
+        match DoubleWrapper::parse_from_bytes(buffer) {
             Ok(result) => Ok(result.get_value()),
             Err(result) => Err(result.to_string()),
         }
