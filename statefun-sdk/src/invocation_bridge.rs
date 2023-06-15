664c0bec2fe500fed70e5dbd5fba38b44a223f8d
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2023-06-15 18:46:44 +0900

    Remove dead code

--- statefun-sdk/src/invocation_bridge.rs
+++ statefun-sdk/src/invocation_bridge.rs
@@ -286,20 +286,6 @@ where
     Ok(())
 }
 
-// /// Unpacks the given state, which is expected to be a serialized `Any<T>`.
-// fn unpack_state<T: Message>(value_spec: ValueSpecBase, packed_state: &Any) -> Option<T> {
-//     // let packed_state: Any =
-//     //     protobuf::parse_from_bytes(serialized_state).expect("Could not deserialize state.");
-
-//     log::debug!("Packed state for {:?}: {:?}", value_spec, packed_state);
-
-//     let unpacked_state: Option<T> = packed_state
-//         .unpack()
-//         .expect("Could not unpack state from Any.");
-
-//     unpacked_state
-// }
-
 #[cfg(test)]
 mod tests {
     use core::time::Duration;
